6f921ca7b104bac43c64739cbc9efc1d3c1e6547
shokha12 <sxaltaev@mail.ru>
2020-08-26 20:01:17 +0500

    Header file

--- templates/header.php
+++ templates/header.php
@@ -1,10 +1,11 @@
 <?php 
   session_start();
-
 ?>
 <head>
-	<title>YAKO</title>
-	    <!-- Compiled and minified CSS -->
+    <meta charset = "utf-8">
+  <title>YAKO</title>
+  <link rel = "icon" href = "image/favicon.ico" type = "image/x-icon">
+      <!-- Compiled and minified CSS -->
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/materialize/1.0.0/css/materialize.min.css">
 
      <!-- Compiled and minified JavaScript -->
@@ -17,29 +18,29 @@
      <!--Let browser know website is optimized for mobile-->
     <meta name="viewport" content="width=device-width, initial-scale=1.0"/>
     <style>
-    	img{
-    		height:300px;
-    	}
-    	nav{
-    		margin-bottom: 15px;
-    	}
-    	#card{
-    		margin-top: 15px;
+      img{
+        height:300px;
+      }
+      nav{
+        margin-bottom: 15px;
+      }
+      #card{
+        margin-top: 15px;
 
-    	}
-    	.textarea{
-  			height: 400px;
-  		}
-  		.newform{
-  			height: 7px;
-  			margin:10px auto;
-  		}
-  		form{
-  		max-width: 660px;
-  		height: 700px;
-  		margin: 20px auto;
-  		padding: 20px;
-  		}
+      }
+      .textarea{
+        height: 400px;
+      }
+      .newform{
+        height: 7px;
+        margin:10px auto;
+      }
+      form{
+      max-width: 660px;
+      height: 700px;
+      margin: 20px auto;
+      padding: 20px;
+      }
       .sform{
         max-width: 500px;
       }
@@ -68,6 +69,15 @@
         text-align: center;
         color: blue;
       }
+    #vk_comments{
+        width:950px;
+    }
+    @media only screen and (max-width: 600px) {
+        #vk_comments{
+            width:500px;
+        }
+    }
+      
 
     </style>
     <!-- Put this script tag to the <head> of your page -->
@@ -76,48 +86,44 @@
     <script type="text/javascript">
       VK.init({apiId: 7553529, onlyWidgets: true});
     </script>
-    
 </head>
 <body class="grey lighten-2">
-	<nav class="nav-wrapper indigo navbar-fixed">
-		<div class="container">
-			<a href="index.php" class="brand-logo">YAKO@blogs</a>
-			<a href="#" class="sidenav-trigger" data-target="mobile-links">
-				<i class="material-icons">menu</i>
-			</a>
-      
-			<ul class="right hide-on-med-and-down">
-				<li><a href="index.php">Home</a></li>
-				<li><a href="about.php">About</a></li>
-				<li><a href="signup.php">Sign up</a></li>
-				<li><a href="register.php">Log in</a></li>
-        <?php if(isset($_SESSION['login'])){ ?>
-				  <li><span><a href="myprofile.php?id=<?php echo $_SESSION['id']; ?>" class="right grey-text">
-					 <i class="large material-icons">account_circle</i>
-				  </a></span></li>
-        <?php }?>
-        <li><a href="logout.php" class="btn red">Logout</a></li>
-        <li class="white-text">
+  <nav class="nav-wrapper indigo navbar-fixed">
+    <div class="container">
+      <a href="index.php" class="brand-logo">YAKO@blogs</a>
+      <a href="#" class="sidenav-trigger" data-target="mobile-links">
+        <i class="material-icons">menu</i>
+      </a>
+      <ul class="right hide-on-med-and-down">
+        <li><a href="index.php">Home</a></li>
+        <li><a href="about.php">About</a></li>
+        <li><a href="signup.php">Sign up</a></li>
+        <li><a href="register.php">Log in</a></li>
           <?php if(isset($_SESSION['login'])){ ?>
-            Hello, <?php echo $_SESSION['login']; ?>   
-          <?php } ?> 
-        </li>
-			</ul>	
-     
-		</div>
-	</nav>
+          <li><span><a href="myprofile.php?id=<?php echo $_SESSION['id']; ?>" class="right grey-text">
+           <i class="large material-icons">account_circle</i>
+          </a></span></li>
+                <?php }?>
+                <li><a href="logout.php" class="btn red">Logout</a></li>
+                <li class="white-text">
+                <?php if(isset($_SESSION['login'])){ ?>
+                    Hello, <?php echo $_SESSION['login']; ?> !   
+                <?php } ?> </li>
+      </ul> 
+    </div>
+  </nav>
 
-	<ul class="sidenav" id="mobile-links">
-		<li><span><a href="account.html" class="right grey-text">
-			<i class="medium material-icons">account_circle</i>
-		</a></span></li>
+  <ul class="sidenav" id="mobile-links">
+      <?php if(isset($_SESSION['login'])){ ?>
+        <li><span><a href="myprofile.php?id=<?php echo $_SESSION['id'];?>" class="right grey-text">
+          <i class="medium material-icons">account_circle</i>
+        </a></span></li>
+        <li><a href="myprofile.php?id=<?php echo $_SESSION['id'];?>">My profile</a>
+    <?php }?>
+    <li><a href="index.php">Home</a></li>
+    <li><a href="about.php">About</a></li>
+    <li><a href="signup.php">Sign up</a></li>
     <li><a href="register.php">Log in</a></li>
-		<li><a href="index.php">Home</a></li>
-		<li><a href="about.php">About</a></li>
-		<li><a href="signup.php">Sign up</a></li>
-    <?php if(isset($_SESSION['login'])){ ?>
-      <li><a href="myprofile.php">My profile</a>
-		  <?php }?>
-    <li><a href="logout.php" class="btn red">Logout</a></li>
+        <li><a href="logout.php" class="btn red">Logout</a></li>
   
-	</ul>
+  </ul>
